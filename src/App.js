d9c04c2b6b39f7d418d778f02e9167ea285de3b3
cha9cha9 <mehdigharbi150@gmail.com>
2022-01-24 18:37:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
+import Card01 from './component/Card';
+
+
+
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={show:true}
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.show && <Card01/>}
+        <Button onClick={()=>this.setState({show:!this.state.show})}>SHOW/HIDE</Button>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
